c9edead271c957561c8e880c2cacdd96ac3af656
Henry Fradley <fradleyhenry@gmail.com>
2020-12-16 16:01:18 -0800

    deleted http

--- redis.config.js
+++ redis.config.js
@@ -2,7 +2,7 @@ const Promise = require('bluebird');
 let redis = Promise.promisifyAll(require('redis'));
 const client = redis.createClient({
   port: 6379,
-  host: 'http://34.213.186.178',
+  host: '34.213.186.178',
 });
 
 module.exports = client;
