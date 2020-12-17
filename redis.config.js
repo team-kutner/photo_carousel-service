f7226a52346c406c00c1f9fd64db6cc91770736f
Henry Fradley <fradleyhenry@gmail.com>
2020-12-16 16:17:43 -0800

    modified promisify

--- redis.config.js
+++ redis.config.js
@@ -1,5 +1,5 @@
 const Promise = require('bluebird');
-let redis = Promise.promisifyAll(require('redis'));
+Promise.promisifyAll(require('redis'));
 const client = redis.createClient({
   port: 6379,
   host: '34.213.186.178',
