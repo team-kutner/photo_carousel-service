e1162096135aa1e791a9f1a3c6accc9a275124f3
Henry Fradley <fradleyhenry@gmail.com>
2020-12-16 17:30:06 -0800

    made new redis

--- redis.config.js
+++ redis.config.js
@@ -2,7 +2,7 @@ const Promise = require('bluebird');
 let redis = Promise.promisifyAll(require('redis'));
 const client = redis.createClient({
   port: 6379,
-  host: '34.213.186.178',
+  host: '34.217.149.1',
 });
 
 module.exports = client;
